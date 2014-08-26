f81196902a229ae4a22b7aa3e65e4c3a6734117f
Marcelo Garlet Millani <marcelogmillani@gmail.com>
2014-08-26 12:50:25 +0200

    Added tests for every machine

---
+++ tests/program/runtest.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+fname(){
+	echo "$1" | sed 's:\..*::'
+}
+
+assemble(){
+	IFILE="$1"
+	MACHINE="$2"
+	FILEN=$(fname $1)
+	hephasmos --input "$IFILE" --machine "$MACHINE" --messages asmmessages --output "$FILEN.mem" --type Mecaria0 --symbols "$FILEN.sym"
+}
+
+files=(ahmes.ahm cromag.cmg neander.nea pericles.per pitagoras.pit queops.que ramses.rhm reg.reg volta.vlt numbers.pit)
+machines=(ahmes cromag neander pericles pitagoras queops ramses reg volta pitagoras)
+
+for ((i=0 ; i < ${#files[@]} ; i++))
+do
+	echo "${files[$i]}"
+	assemble "${files[$i]}" "machines/${machines[$i]}"
+	name=$(fname "${files[$i]}")	
+	diff "$name.ok" "$name.mem"
+	echo '------------'
+done
+
